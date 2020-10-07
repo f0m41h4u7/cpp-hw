54d364e4ec96cc286eb3710c5d101fbd64d7962e
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-10-07 22:00:03 +0300

    change include

--- ip_filter/ip_filter.hpp
+++ ip_filter/ip_filter.hpp
@@ -1,7 +1,7 @@
 #include <arpa/inet.h>
 #include <regex>
 #include <set>
-#include <string_view>
+#include <string>
 
 const std::regex IPv4Regex("((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
 
