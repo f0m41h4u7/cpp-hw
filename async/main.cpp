251464b8b1e6680587773ee30e62ca13b9ce3271
Anna Berezina <anna.berezina@niuitmo.ru>
2021-02-18 10:02:19 +0300

    Update main.cpp

--- async/main.cpp
+++ async/main.cpp
@@ -1,31 +1,7 @@
-#include <iostream>
-
 #include "async.hpp"
 
-int main()//int argc, char** argv)
-{
-  /*
-  if(argc != 2)
-  {
-    std::cout << "require block size as argument\n";
-    return 1;
-  }
-
-  int n = 0;
-  if(!sscanf(argv[1], "%d", &n))
-  {
-    std::cout << "argument should be int\n";
-    return 1;
-  }
-
-  std::string line;
-  bulk bulk(n);
-
-  while(std::getline(std::cin, line))
-    bulk.add(line);
-
-  bulk.print();*/
-  
+int main()
+{ 
   auto handler = async_blk::connect(3);
   
   async_blk::receive(handler, "cmd1\ncmd2");
